521885c64d94287c574d775b5329cc23d4802247
wang048 <wang048@gmail.com>
2025-06-09 16:55:36 +0800

    Fix App.jsx template literal syntax error

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,11 @@ export default function App() {
                 {q.options.map((opt, idx) => (
                   <button
                     key={idx}
-                    className={\`p-2 rounded border \${answers[i] === idx ? 'bg-blue-100 border-blue-500' : 'border-gray-300'}\`}
+                    className={
+  "p-2 rounded border " +
+  (answers[i] === idx ? "bg-blue-100 border-blue-500" : "border-gray-300")
+}
+
                     onClick={() => handleAnswer(i, idx)}
                   >
                     {opt}
